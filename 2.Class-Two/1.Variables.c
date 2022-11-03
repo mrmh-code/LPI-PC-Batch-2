38cdca62287232ec612cef549a38d959b64a6f1f
mrmh-code <hasanalmehedi07@gmail.com>
2022-11-03 22:04:39 +0600

    [Add] Variables

---
+++ 2.Class-Two/1.Variables.c
@@ -0,0 +1,57 @@
+#include <stdio.h> //header file
+
+int main() 
+{
+
+    // int part code
+    int num;  // Declaration
+    num = 50; // definition
+
+    // printf("num = %d \n",num);
+
+    // printf("int size of %d \n",sizeof(int));
+
+    // int a=50;
+    // int b=30;
+
+    // printf("sum = %d\n",a+b);
+    // printf("a=%d \t b=%d\n", a,b);
+
+    // float part code
+    // float x=1.123456;
+    // printf("%.2f\n",x);
+    // printf("float size of : %d\n",sizeof(float));//4
+
+    // double part code
+    //  double pi=3;
+    //  printf("%.3lf\n",pi);
+    //  printf("double size of : %d\n",sizeof(double));//8
+
+    // char part
+    // char ch='a';
+    // printf("%c\n",ch);
+    //   printf("char size of : %d\n",sizeof(ch));
+
+    // example - why variable need
+    int price = 12;
+    // printf("%d\n",price);
+    // printf("%d\n",price+5);
+    // printf("%d\n",price+3);
+    //  printf("%d\n",price+10);
+
+    //  printf("%d\n",15);
+    // printf("%d\n",15+5);
+    // printf("%d\n",15+3);
+    //  printf("%d\n",15+10);
+
+    //* 2*2=4
+    // a/b 10/2=5
+    // + - 2+2=4 2-2=0
+
+    int s1, s2, s3;
+    int a = 10;
+
+    s1 = 10, s2 = 20, s3 = 30;
+    int sum = s1 + s2 + s3;
+    printf("sum= %d\n", sum);
+}
