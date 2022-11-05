79a6580949204a6076a691d51ccc400c6b290fc1
mrmh-code <hasanalmehedi07@gmail.com>
2022-11-05 23:48:22 +0600

    [Add] take input user

---
+++ 3.Class-Three/1.input_output.c
@@ -0,0 +1,16 @@
+#include <stdio.h> //header file
+
+int main()
+{
+    // Declaration variables
+    int a, b;
+    
+    // user input
+    scanf("%d %d", &a, &b);
+
+    // sum of a and b
+    printf("sum =%d\n", a + b);
+
+    // Subtract b from a
+    printf("subtract =%d\n", b - a);
+}
