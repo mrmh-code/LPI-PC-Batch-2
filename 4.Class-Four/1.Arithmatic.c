e9da8945f0c9fb0433f75a04b3a99098b52e0bc4
mrmh-code <hasanalmehedi07@gmail.com>
2022-11-07 21:57:54 +0600

    Arithmetic & Relational Operator

---
+++ 4.Class-Four/1.Arithmatic.c
@@ -0,0 +1,53 @@
+#include <stdio.h> //header file
+
+int main()
+{
+
+    // Addition
+    // int a=10,b=20;
+    // printf("sum= %d\n",a+b);
+
+    // Subtraction
+    //  int a=20,b=10,c=50;
+    //  printf("subtraction =%d\n",a+b-c);
+
+    // Multiplication
+    //  int a=20, b=10;
+
+    // printf("%d\n",a*b);
+
+    // DiVision
+    //  float a=10,b=20;
+    //  printf("Division %f\n",a/b);
+
+    // modulus
+    //  int a=10,b=20;
+    //  printf("%d\n",a%b);
+    //  printf("%d\n",b%a);
+    //  printf("%d\n",10%3);
+    //  printf("%f",10.2%2.1);
+
+    // greater than (>) (<)
+
+    // printf("10>20 = %d\n",10>20);
+    // printf("20>10 = %d\n",20>10);
+
+    /// less than
+    // int a=10,b=20;
+    // printf("a<b = %d\n",a<b);
+    // printf("b<a = %d\n",b<a);
+
+    // greater than or equal to
+    //   int a=10,b=10;
+    //  printf("%d\n",a>=b);
+
+    // less than or equal to
+    //  int a=20,b=20;
+    //  printf("%d",a<=b);
+
+    // not equal to !=
+    // printf("%d\n",!(10!=11));
+
+    // equal to ==
+    //  printf("%d\n",10==12);
+}
