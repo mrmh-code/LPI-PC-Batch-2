bc984f0a10c5a6aee687226a36da6967ea08f72c
ShafiMunshi <69038196+ShafiMunshi@users.noreply.github.com>
2022-11-12 21:31:15 +0600

    Add files via upload

---
+++ Class6.c
@@ -0,0 +1,115 @@
+// Bismillahir Rahmanir Rahim
+
+#include <stdio.h>
+
+int main()
+{
+
+    //    int height,weight;
+    //    printf("Enter your height: ");
+    //    scanf("%d",&height);
+    //    printf("Enter your weight: ");
+    //    scanf("%d",weight);
+
+    //   if(height>5.7 && weight>50){
+    //     printf("You can join the army");
+    //   }else{
+    //     printf("You cannot join the army");
+    //   }
+
+    //     int num;
+    //     printf("Enter your number: ");
+    //     scanf("%d",&num);
+
+    //    if(44>33){
+    //     printf("You have passed");
+    //    }else{
+    //     printf("You have failed");
+    //    }
+
+    // printf("%d",44>33);
+
+    // result marksheet.
+
+    // int result;
+    // printf("Enter your result: ");
+    // scanf("%d", &result);
+
+    // if (result >= 80)
+    // {
+    //     printf("You have got A+");
+    // }
+    // else if (result >= 70)
+    // {
+    //     printf("You have got A");
+    // }
+    // else if (result >= 60)
+    // {
+    //     printf("You have got A-");
+    // }
+    // else if (result >= 50)
+    // {
+    //     printf("You have got B");
+    // }
+    // else if (result >= 40)
+    // {
+    //     printf("You have got C");
+    // }
+    // else if (result >= 33)
+    // {
+    //     printf("You have got D");
+    // }
+    // else
+    // {
+    //     printf("You have failed");
+    // }
+
+    // largest number between three number..
+
+    // int a,b,c;
+
+    // printf("Enter your A: ");
+    // scanf("%d",&a);
+    // printf("Enter your B: ");
+    // scanf("%d",&b);
+    // printf("Enter your C: ");
+    // scanf("%d",&c);
+
+    // if(a>b){
+    //    // if a>b this code will executed
+
+    //     if(a>c){
+    //         printf("A is the largest number");
+    //     }else{
+    //         printf("C is the largest number.");
+    //     }
+
+    // }else{
+    //     // b>a this code will executed
+    //     if(b>c){
+    //         printf("B is largest number");
+    //     }else{
+    //         printf("C is largest number");
+    //     }
+    // }
+
+    // even or odd/
+
+    //     int num;
+    //     scanf("%d",&num);
+
+    //    if(num%2 != 0){
+    //     printf("It is odd number");
+    //    }else{
+    //     printf("It is even number");
+    //    }
+
+    int x;
+
+    printf("enter x: ");
+    scanf("%d", &x);
+
+    printf("Result is %d",x==50 || x==60);
+
+    return 0;
+}
