ead38fc9c05d539a270348e24883dd80ecf34709
ShafiMunshi <69038196+ShafiMunshi@users.noreply.github.com>
2022-11-14 21:16:07 +0600

    Add files via upload

---
+++ class7.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // Switch statement
+
+    // question 2
+
+    // int a,b,c;
+
+    // printf("Enter your first number: ");
+    // scanf("%d",&a);
+    //  printf("Enter your second number: ");
+    // scanf("%d",&b);
+    //  printf("Enter your third number: ");
+    // scanf("%d",&c);
+
+    // if(a==b && b==c){
+    //     printf("The triangle is equal");
+    // }else if(a==b || b==c || c==a){
+    //     printf("The triangle is icoseles");
+    // }else{
+    //     printf("The triangle is not equal");
+    // }
+
+    // int num = 4;
+
+    switch(8){
+        case 1:
+            printf("Good Morning");
+            break;
+
+        case 2:
+            printf("Good afternoon");
+            break;
+
+        case 3:
+            printf("Good Night");
+            break;
+
+        case 4:
+            printf("Goood boy");
+            break;
+
+        default:
+            printf("No cases matched");
+            break;
+
+    }
+
+    // char ch;
+    // scanf("%c", &ch);
+
+    // switch (ch)
+    // {
+    // case 'a':
+    // case 'e':
+    // case 'i':
+    // case 'o':
+    // case 'u':
+    //     printf("The character is vowel");
+    //     break;
+
+    // default:
+    //     printf("The character is consonent");
+    //     break;
+    // }
+
+    // int num1, num2, ans;
+
+    // char ch;
+    // printf("What you want to do now? : ");
+    // scanf("%c", &ch);
+
+    // switch (ch)
+    // {
+    // case '+':
+
+    //     printf("Enter your first number: ");
+    //     scanf("%d", &num1);
+    //     printf("Enter your second number: ");
+    //     scanf("%d", &num2);
+
+    //     printf("%d\n", num1 + num2);
+    //     break;
+
+    // case '-':
+    //     printf("Enter your first number: ");
+    //     scanf("%d", &num1);
+    //     printf("Enter your second number: ");
+    //     scanf("%d", &num2);
+    //     ans = num1 - num2;
+    //     printf("%d\n", ans);
+    //     break;
+    // case '*':
+    //     printf("Enter your first number: ");
+    //     scanf("%d", &num1);
+    //     printf("Enter your second number: ");
+    //     scanf("%d", &num2);
+    //     ans = num1 * num2;
+    //     printf("%d\n", ans);
+    //     break;
+    // case '/':
+    //     printf("Enter your first number: ");
+    //     scanf("%d", &num1);
+    //     printf("Enter your second number: ");
+    //     scanf("%d", &num2);
+    //     ans = num1 / num2; // ans = 5
+    //     printf("%d\n", ans);
+    //     break;
+
+    // default:
+    //     printf("Invalid ");
+    //     break;
+    // }
+
+    return 0;
+}
