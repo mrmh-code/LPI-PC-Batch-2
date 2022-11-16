38b3872bdc6836f897c31b59f11b605773e1aaab
ShafiMunshi <69038196+ShafiMunshi@users.noreply.github.com>
2022-11-16 21:42:44 +0600

    Add files via upload

---
+++ class8.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+int main()
+{
+    
+
+    // printf("Hello\n");
+    // printf("Hello\n");
+    // printf("Hello\n");
+    // printf("Hello\n");
+    // printf("Hello\n");
+    // printf("Hello\n");
+
+    // a=a+1;
+
+    // a++;
+
+    // a = a-1;
+
+    // a--;
+
+    // int a = 10;
+    // printf("%d\n", a);
+
+    // // a = a + 5;
+
+    // a /= 5;
+
+    // // a++;
+
+    // printf("%d\n", a);
+
+    // for (int i = 5; i <= 10; i++)
+    // {
+    //     printf("%d\n",i);
+    // }
+
+    // for (int i = 50; i >= 1; i--)
+    // {
+    //     printf("%d\n", i);
+    // }
+
+    // sum of 1 to n
+
+    // for (int i = 0; i <= 100; i++)
+    // {
+
+    // }
+
+    int n = 100;
+
+    // int ans = (n*(n+1))/2;
+
+    // printf("%d",ans);
+
+    int ans = 0;
+
+    for (int i = 1; i <= 5; i++)
+    {
+        ans = ans + i; 
+    }
+
+    printf("Sum of 1 to 5 = %d",ans);
+
+ 
+
+
+    return 0;
+}
